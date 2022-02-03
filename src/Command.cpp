32eceddd07bb471d915cd9985e42969112871b9c
Toad Miranda <tmiranda@pr-a5.kzn.21-school.ru>
2022-02-03 16:47:54 +0300

    edit help

--- src/Command.cpp
+++ src/Command.cpp
@@ -41,37 +41,37 @@ void	Command::checkCommand(Server &server){
 void	Command::doHelpCommand(){
 	send(_fd, "COMMANDS:\n", 10, 0);
 	send(_fd, "NICK\n", 5, 0);
-	send(_fd, "    Syntax:\n", 12, 0);
+	send(_fd, "   Syntax:\n", 11, 0);
 	send(_fd, "   NICK <nickname>\n", 19, 0);
 	send(_fd, "   Allows a client to change their IRC nickname.\n", 49, 0);
 	send(_fd, "QUIT\n", 5, 0);
-	send(_fd, "    Syntax:\n", 12, 0);
-	send(_fd, "  QUIT [<message>]\n", 20, 0);
-	send(_fd, "  Disconnects the user from the server.\n", 41, 0);
+	send(_fd, "   Syntax:\n", 11, 0);
+	send(_fd, "   QUIT [<message>]\n", 20, 0);
+	send(_fd, "   Disconnects the user from the server.\n", 41, 0);
 	send(_fd, "JOIN\n", 5, 0);
-	send(_fd, "    Syntax:\n", 12, 0);
-	send(_fd, "  JOIN <channels> [<keys>]\n", 28, 0);
+	send(_fd, "   Syntax:\n", 11, 0);
+	send(_fd, "   JOIN <channels> [<keys>]\n", 28, 0);
 	send(_fd, "   Makes the client join the channels in the comma-separated list <channels>, specifying the passwords, if needed, in the comma-separated list <keys>. If the channel(s) do not exist then they will be created.\n", 209, 0);
 	send(_fd, "PRIVMSG\n", 8, 0);
-	send(_fd, "    Syntax:\n", 12, 0);
-	send(_fd, "  PRIVMSG <msgtarget> :<message>\n", 34, 0);
-	send(_fd, "  Sends <message> to <msgtarget>, which is usually a user or channel.\n", 71, 0);
+	send(_fd, "   Syntax:\n", 11,	0);	
+	send(_fd, "   PRIVMSG <msgtarget> :<message>\n", 34, 0);
+	send(_fd, "   Sends <message> to <msgtarget>, which is usually a user or channel.\n", 71, 0);
 	send(_fd, "NOTICE\n", 7, 0);
-	send(_fd, "    Syntax:\n", 12, 0);
-	send(_fd, "  NOTICE <msgtarget> <message>\n", 32, 0);
-	send(_fd, "  This command works similarly to PRIVMSG, except automatic replies must never be sent in reply to NOTICE messages.\n", 117, 0);
+	send(_fd, "   Syntax:\n", 11, 0);
+	send(_fd, "   NOTICE <msgtarget> <message>\n", 32, 0);
+	send(_fd, "   This command works similarly to PRIVMSG, except automatic replies must never be sent in reply to NOTICE messages.\n", 117, 0);
 	send(_fd, "PART\n", 5, 0);
-	send(_fd, "    Syntax:\n", 12, 0);
-	send(_fd, "  PART <channels> [<message>]\n", 31, 0);
-	send(_fd, "  Sets a connection password. This command must be sent before the NICK/USER registration combination.\n", 104, 0);
+	send(_fd, "   Syntax:\n", 11, 0);
+	send(_fd, "   PART <channels> [<message>]\n", 31, 0);
+	send(_fd, "   Sets a connection password. This command must be sent before the NICK/USER registration combination.\n", 104, 0);
 	send(_fd, "KICK\n", 5, 0);
-	send(_fd, "    Syntax:\n", 12, 0);
-	send(_fd, "  KICK <channel> <client> :[<message>]\n", 40, 0);
-	send(_fd, "  Forcibly removes <client> from <channel>. This command may only be issued by channel operators.\n", 99, 0);
+	send(_fd, "   Syntax:\n", 11, 0);
+	send(_fd, "   KICK <channel> <client> :[<message>]\n", 40, 0);
+	send(_fd, "   Forcibly removes <client> from <channel>. This command may only be issued by channel operators.\n", 99, 0);
 	send(_fd, "BOT\n", 4, 0);
-	send(_fd, "    Syntax:\n", 12, 0);
-	send(_fd, "  BOT HELP\n", 12, 0);
-	send(_fd, "  Рelps clients navigate in commands\n", 38, 0);
+	send(_fd, "   Syntax:\n", 11, 0);
+	send(_fd, "   BOT HELP\n", 12, 0);
+	send(_fd, "   Рelps clients navigate in commands\n", 38, 0);
 }
 
 void	Command::doQuitCommand(){
